e9198be9ccca7fe902e10add584469e361ead2c2
Galen Strasen <galen@wearetmbr.com>
2016-10-13 16:34:43 -0700

    Messing around with flex

---
+++ wp-content/themes/shespeaks/partials/flex-ex-3.php
@@ -0,0 +1,33 @@
+<header class="main-header">
+    <h1 class="name"><a href="#">Best City Guide</a></h1>
+    <ul class="main-nav">
+      <li><a href="#">ice cream</a></li>
+      <li><a href="#">donuts</a></li>
+      <li><a href="#">tea</a></li>
+      <li><a href="#">coffee</a></li>
+    </ul>
+  </header><!--/.main-header-->
+
+  <div class="banner">
+    <img class="logo" src="/wp-content/themes/shespeaks/partials/img/city-logo.svg" alt="City">
+    <h1 class="headline">The Best City</h1>
+    <span class="tagline">The best drinks and eats in the best city ever.</span>
+  </div><!--/.banner-->
+
+  <div class="row-thing">
+    <div class="primary col">
+      <h2>Welcome!</h2>
+      <p>Everything in this city is worth waiting in line for!</p>
+    </div><!--/.primary-->
+  </div>
+
+  <footer class="main-footer">
+    <div class="footer-inner">
+      <span>&copy;2015 Residents of The Best City Ever.</span>
+    </div>
+  </footer>
+
+  <?php wp_footer(); ?>
+
+</body>
+</html>
