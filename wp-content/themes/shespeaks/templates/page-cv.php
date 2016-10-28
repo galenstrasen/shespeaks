de76bbb7951dd50c86789927f9d56ae5780d4192
Galen Strasen <galen@wearetmbr.com>
2016-10-28 16:52:07 -0700

    CV content

--- wp-content/themes/shespeaks/templates/page-cv.php
+++ wp-content/themes/shespeaks/templates/page-cv.php
@@ -8,14 +8,152 @@
     <div class="row">
 
         <div class="col-sm-12">
+        <article class="entry-content">
           <header class="cv-header">
-          <h2 class="hero">She Speaks in Code</h2>
-          <h1 class="me">Galen Strasen</h1>
-          <h5 class="contact">917-816-5093 | <a href="">gstrasen@gmail.com</a><br/>4138 Webster Street #7, Oakland CA 94609</h5>
+          <h2 class="hero hidden">She Speaks in Code</h2>
+          <h1 class="me hero">Galen Strasen</h1>
+          <p class="contact">917-816-5093 | <a href="">gstrasen@gmail.com</a><br/>4138 Webster Street #7, Oakland CA 94609</p>
+          <a href="">I'm overwhelmed, take me to the quick & dirty</a>
           </header>
+          <section class="cv-section -work">
+            <h2 class="heading">Relevant Experience</h2>
+            <div class="cv-block">
+              <h4 class="highlight"><span>We Are TMBR</span></h4>
+              <h3>Web Developer / Digital Producer</h3>
+              <h5>May 2013 - Present</h5>
+              <p>Worked both in-office and remotely with a small but agile team of Wyoming based creatives to drive both the discovery and development phases of the web project life cycle.</p>
+              <p>Key responsibilities include:</p>
+              <ul class="plus -bulky">
+                <li>Interfacing with client and team members to identify inital project needs, scope functionality requirements, map site architecture and provide recommendations on overall web strategy based on project goals and industry trends.</li>
+                <li>Building, testing, and maintaining standards-compliant, performance optimized, custom responsive WordPress websites from the ground up, while keeping in mind SEO best practices, browser idiosynchronicities and the needs of the end user (among other things).</li>
+                <li>Architecting intuitive and structurally sound content management systems that would allow the client full autonomy over their new site. </li>
+                <li>Being comfortable stepping into the role of project manager and/or designer as needed to free up resources.</li>
+                <li>Participating in daily team-wide scrum.</li>
+                <li>Driving developement of TMBR's internal code libraries and WP starter theme.</li>
+                <li>Initiating and actively participating in conversations about process and coding best practices.</li>
+              </ul>
+            </div>
 
+            <div class="cv-block">
+            <h4 class="highlight"><span>VR Interactive</span></h4>
+            <h3>Front End Developer / SEO Specialist</h3>
+            <h5>Jackson Hole, WY - June 2009 - May 2013</h5>
+            <p>Worked in a variety of areas for a tiny web development shop in Jackson Hole, picking up skills quickly so as to competently wear any hat that needed to be worn in a given moment. </p>
+            <ul class="plus -bulky">
+              <li>Being prepared to interact with client web projects / web content as front end developer, content manager, designer or ground-up developer.</li>
+              <li>Familiarizing myself with both WordPress and our custom .NET CMS and being ready willing and able to efficiently debug / contribute to code. </li>
+              <li>Designed a comprehensive SEO package that I was tasked with executing as well. </li>
+              <li>Performing SEO audits of websites, recommending improvements, performing keyword research, compiling monthly seo performance reports, </li>
+              <li>Tracking performance in Google analytics</li>
+              <li>Driving paid advertising campaigns using google adwords</li>
+              <li>Designed a social media services packages</li>
+              <li>Content writing ( I wrote content for everything from legal and financial websites to websites specializing in nordic ski gear)</li>
+              <li>Designed and created email templates </li>
+            </ul>
+            </div>
+          </section>
+          <section class="cv-section">
+            <h2 class="heading">Core Competencies</h2>
+            <div class="cv-block">
+            <h4 class="highlight"><span>Responsive Front End Development</span></h4>
+            <p>On a very basic level, I bring the web to life. From a 2-D mockup, I can build a webpage that maintains functionality and aesthetic across devices.  I have a keen eye for detail and am a stickler for standards compliant code. I pride myself in my ability to recognize the latest and greatest trends in front end design and always do whatever I can to keep abreast of the latest technologies. </p>
+            <ul class="plus">
+              <li>7+ years working in, on and around websites.</li>
+              <li>Fluent in W3C compliant HTML/HTML5</li>
+              <li>Deep understanding of (and genuine love for) CSS3/CSS</li>
+              <li>Knowledge of object-oriented approach to CSS (currently obssessed with atomic design system)</li>
+              <li>Highly versed in responsive development including on-device remote debugging and testing across browsers and operating systems</li>
+              <li>Extensive experience troubleshooting cross-browser issues and with QA process</li>
+              <li>Versed in front-end frameworks (think Bootstrap). </li>
+              <li>High level knowledge of PHP and PHP/MySQL web server environments</li>
+              <li>Working knowledge of Javascript, jQuery and working with apis via javascript </li>
+              <li>Experience integrating backend technologies such as json</li>
+              <li>Familiarity with Git/GitHub-based workflow</li>
+              <li>Solid experience working with package management tools (such as npm and bower) as well as front-end compiling tools (SCSS and gulp)</li>
+              <li>Knowledge of both Mac OS X and Microsoft Windows operating systems</li>
+              <li>Solid experience with Adobe Photoshop and best practices for web ready images</li>
+              <li>Working knowledge of Adobe Illustrator and crating and exporting web-gready graphics such as svgs </li>
+              </ul>
+              </div>
+              <div class="cv-block">
+            <h4 class="highlight"><span>WordPress Development</span></h4>
+            <p>Simply speaking, I am passionate about WordPress. I have over 4 years of hands-on experience bringing WordPress websites from concept to completion, whether building themes from the ground up or expanding upon / customizing existing themes. In addition I have worked with clients to organize and design content management systems that are intuitive and educated WP newbies on how best to use the dashboard. I have a working knowledge of the WordPress core and architecture, and always keep my ear to the WordPress community for news on the latest plugins, bugs and fixes. </p>
+            <ul class="plus">
+              <li>5+ years building custom wordpress themes.</li>
+              <li>High level knowledge of PHP and PHP/MySQL web server environments</li>
+              <li>Familarity with WP core - plugins etc - WP solutions</li>
+            </ul>
+            </div>
+            <div class="cv-block">
+            <h4 class="highlight"><span>Digital Production</span></h4>
+            <p>A website is so much more than a static image rendered in code - a website is a system of information. I have worked with clients and team members to identify client goals, target demographics and formulate content inventories with the goal of outlining all client needs before the development process begins. This ensures that developers, designers and project managers remain in stride with client expectations and helps determine accurate project scope early on. As I have streamlined this process, it has become an invaluable component of the design, develop, deploy lifecycle.</p>
+              <ul class="plus">
+              <li>Strong written and oral communication, in particular communicating tech jargon into a language clients can understand.</li>
+              <li>Experience with agile scrum process</li>
+              </ul>
+              </div>
+          </section>
+          <section class="cv-section">
+          <div class="cv-block">
+            <h2 class="heading">Skillz</h2>
+            <ul class="plus">
+              <li>7+ years working in, on and around websites.</li>
+              <li>5+ years building custom wordpress themes.</li>
+              <li>Fluent in W3C compliant HTML/HTML5</li>
+              <li>Deep understanding of (and genuine love for) CSS3/CSS</li>
+              <li>Knowledge of object-oriented approach to CSS (currently obssessed with atomic design system)</li>
+              <li>Highly versed in responsive development including on-device remote debugging and testing across browsers and operating systems</li>
+              <li>Extensive experience troubleshooting cross-browser issues and with QA process</li>
+              <li>Versed in front-end frameworks (think Bootstrap). </li>
 
+              <li>High level knowledge of PHP and PHP/MySQL web server environments</li>
+              <li>Working knowledge of Javascript, jQuery and working with apis via javascript </li>
+              <li>Experience integrating backend technologies such as json</li>
+              <li>Familiarity with Git/GitHub-based workflow</li>
+              <li>Solid experience working with package management tools (such as npm and bower) as well as front-end compiling tools (SCSS and gulp)</li>
+              <li>Knowledge of both Mac OS X and Microsoft Windows operating systems</li>
+              <li>Solid experience with Adobe Photoshop and best practices for web ready images</li>
+              <li>Working knowledge of Adobe Illustrator and crating and exporting web-gready graphics such as svgs </li>
+              <li>Strong written and oral communication, in particular communicating tech jargon into a language clients can understand.</li>
+              <li>Experience with agile scrum process</li>
 
+            </ul>
+            </div>
+          </section>
+             <section class="cv-section hidden">
+            <h2 class="heading">Less Relevant Experience</h2>
+            <h4><strong>We Are TMBR</strong> &mdash; Jackson, Wyoming</h4>
+            <h3>Web Developer / Digital Producer</h3>
+            <h5>Remote - May 2013 - present</h5>
+<h3>Social Media Intern</h3>
+            <h5>Jackson Hole, WY - June 2009 - May 2013</h5>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+            <hr/>
+            </section>
+          <section class="cv-section">
+            <h2 class="heading">Education</h2>
+            <div class="cv-block">
+            <h4 class="highlight"><span>Occidental College</span></h4>
+            <h3>Bachelors, Art History and the Visual Arts</h3>
+            <h5>2004 - 2008</h5>
+
+            <h4 class="highlight"><span>United Nations International School</span></h4>
+            <h3>International Baccalaureate, English, Anthrolopology, Theater Arts</h3>
+            <h5>1991 - 2004</h5>
+            </div>
+          </section>
+          <section class="cv-section hidden">
+            <h2 class="heading">Fun Facts</h2>
+            <h4>These are a few of my favorite things / Libraries I could not live without:</h4>
+            <ul class="plus">
+              <li>ACF</li>
+              <li>Slick.js</li>
+              <li>Magnific popup</li>
+              <li>Chris Coyier</li>
+            </ul>
+            <p>chris coyier and whats his face are my celebrity crushes.</p>
+          </section>
+</article>
         </div><!-- /col -->
 
 
