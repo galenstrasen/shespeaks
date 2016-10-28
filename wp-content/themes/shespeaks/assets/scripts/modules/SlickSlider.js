1d8a283f35de48532f0f24517ed674d1819b2746
Galen Strasen <galen@wearetmbr.com>
2016-10-28 12:33:56 -0700

    Modular JS

---
+++ wp-content/themes/shespeaks/assets/scripts/modules/SlickSlider.js
@@ -0,0 +1,48 @@
+// TMBR Creative Agency
+// Author: galen.strasen
+// Date: 7.19.2016
+// Dependent Upon
+// - jQuery
+// - slick slider
+// Module(s) (static)
+// - slick slider
+//
+var SlickSlider = function(element) { // ----- static module
+
+    var _init = function() {
+        // $('.js-slider').slick({
+        //     dots: true,
+        //     arrows: false
+        // });
+        // $('.js-lazy').slick({
+        //     lazyLoad: 'ondemand',
+        //     dots: true,
+        //     arrows: false
+        // });
+        $('.js-hero-slider').slick({
+            autoplay: true,
+            autoplaySpeed: 3000,
+            speed: 3000,
+            arrows: true,
+            fade: true,
+            dots: true
+        });
+        // $('.js-slider-auto').slick({
+        //     autoplay: true,
+        //     autoplaySpeed: 5000,
+        //     dots: true,
+        //     arrows: false
+        // });
+        // $('.js-adaptive-slider').slick({
+        //     autoplay: true,
+        //     autoplaySpeed: 5000,
+        //     dots: true,
+        //     arrows: false,
+        //     adaptiveHeight: true
+        // });
+    };
+    // output/public
+    return {
+        init: _init
+    };
+}(jQuery);
