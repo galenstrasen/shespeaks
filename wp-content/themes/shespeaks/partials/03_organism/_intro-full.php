908a7f77e7842619193522b10e8890fc3052af2a
Galen Strasen <galen@wearetmbr.com>
2016-10-31 16:52:42 -0700

    Intro partials

---
+++ wp-content/themes/shespeaks/partials/03_organism/_intro-full.php
@@ -0,0 +1,34 @@
+
+
+<section class="pg-section section-intro -full">
+  <!-- <div class="container-fluid no-gutter">
+    <div class="row">
+      <div class="col-md-11 col-md-offset-1"> -->
+        <div class="js-hero-slider intro-vibes -slider">
+          <div class="item">
+            <div class="img-bg" style="background-image: url('<?php bloginfo('template_directory');?>/public/images/water1.jpg');"></div>
+          </div><!-- /item -->
+          <div class="item">
+            <div class="img-bg" style="background-image: url('<?php bloginfo('template_directory');?>/public/images/water2.jpg');"></div>
+          </div><!-- /item -->
+          <div class="item">
+            <div class="img-bg" style="background-image: url('<?php bloginfo('template_directory');?>/public/images/water3.jpg');"></div>
+          </div><!-- /item -->
+        </div><!-- /her0-slider -->
+<div class="intro-vibes -sm-guy">
+    <div class="img-bg" style="background-image: url('<?php bloginfo('template_directory');?>/public/images/water1.jpg');"></div>
+    </div>
+  <div class="captionx overlay">
+    <div class="hero-wrap">
+    <h2 class="hero highlight">
+      <span>She</span><br/>
+      <span>Speaks in</span><br/>
+      <span>Code</span>
+    </h2>
+    </div>
+
+      <h4 class="sub">Looking for Galen Strasen?</h4>
+      <a href="#hello" class="scroll-to btn btn-black">You Found Me</a>
+
+  </div>
+</section><!-- /pg-section -->
---
+++ wp-content/themes/shespeaks/partials/03_organism/_intro-half.php
@@ -0,0 +1,19 @@
+
+<section class="pg-section section-intro -half">
+
+  <div class="cv-hero intro-vibes">
+    <div class="img-bg animation" data-animation="animation-fade-in-down" style="background-image: url('<?php bloginfo('template_directory');?>/public/images/water1.jpg');"></div>
+
+    <div class="overlay">
+      <h1 class="hero highlight animation" data-animation="animation-fade-in-left">
+        <span>Galen</span><br/>
+        <span>Strasen</span>
+      </h1>
+    </div><!-- /overlay -->
+
+  </div><!-- /intro-vibes -->
+
+  <div class="clearfix"></div>
+
+</section><!-- /pg-section -->
+
