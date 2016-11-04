a98cef80cfd763e3ebaf18a94c4fc3363790f74d
Galen Strasen <galen@wearetmbr.com>
2016-11-03 17:24:40 -0700

    Scripts

--- wp-content/themes/shespeaks/assets/scripts/Control.js
+++ wp-content/themes/shespeaks/assets/scripts/Control.js
@@ -15,22 +15,22 @@
 */
 var Control = function($) { // ----- static module
     // private var(s)
-    var sliderOptions = {
-        selector:           ".flexslider",
-        namespace:          "flex-",
-        animation:          "slide",
-        slideshow:          false, // auto play on load
-        slideshowSpeed:     2000,
-        animationSpeed:     500,
-        pauseOnHover:       true,
-        controlNav:         false, //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
-        directionNav:       true, //Boolean: Create navigation for previous/next navigation? (true/false)
-        prevText:           "Previous",
-        nextText:           "Next",
-        randomize:          false,
-        touch:              true,
-        video:              true
-    }
+    // var sliderOptions = {
+    //     selector:           ".flexslider",
+    //     namespace:          "flex-",
+    //     animation:          "slide",
+    //     slideshow:          false, // auto play on load
+    //     slideshowSpeed:     2000,
+    //     animationSpeed:     500,
+    //     pauseOnHover:       true,
+    //     controlNav:         false, //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+    //     directionNav:       true, //Boolean: Create navigation for previous/next navigation? (true/false)
+    //     prevText:           "Previous",
+    //     nextText:           "Next",
+    //     randomize:          false,
+    //     touch:              true,
+    //     video:              true
+    // }
 
     // private method(s)
     var _init = function() {
@@ -39,7 +39,8 @@ var Control = function($) { // ----- static module
         //Throttle.init();
         SmoothScroll.init();
         Animated.init();
-        Slider.init(sliderOptions);
+        StickyNav.init();
+        //Slider.init(sliderOptions);
         SlickSlider.init();
         Lightbox.init();
     };
