8c4fe15ebed2ffe4a7a6611b9bbad7cb6239dc2b
Galen Strasen <galen@wearetmbr.com>
2016-11-03 17:25:14 -0700

    Front page markup

--- wp-content/themes/shespeaks/front-page.php
+++ wp-content/themes/shespeaks/front-page.php
@@ -1,26 +1,41 @@
-<?php get_header(); ?>
+<?php
+/** Front Page of Website
+ */
+get_header();
+  while ( have_posts() ) : the_post();
+
+
+  // Hero
+  get_template_part( 'partials/03_organism/_intro-full' );
+
+
+  // Nav
+  get_template_part( 'partials/03_organism/navbar' );
+  ?>
 
 
 
-<main class="pg-main" role="main">
+  <main class="page-main" role="main">
+
+    <section class="pg-section section-about" id="hello">
+      <?php get_template_part( 'partials/about' ); ?>
+    </section>
 
 
-  <?php get_template_part( 'partials/03_organism/_intro-full' ); ?>
-
-  <section class="pg-section section-about" id="hello">
-    <?php get_template_part( 'partials/about' ); ?>
-  </section>
-
-   <section class="pg-section section-deuce">
-    <?php get_template_part( 'partials/style' ); ?>
-    <h1>She Speaks in Code</h1>
-  </section>
+<h4 class="logo">+<<</h4>
 
 
 
+    <section class="pg-section section-recent" id="recent">
+      <?php //get_template_part( 'partials/about' ); ?>
+    </section>
+
+  </main>
+  <!-- #page-main -->
 
 
-</main>
-<!-- #page-main -->
 
-<?php get_footer();
+<?php
+  endwhile;
+edit_post_link();
+get_footer();
