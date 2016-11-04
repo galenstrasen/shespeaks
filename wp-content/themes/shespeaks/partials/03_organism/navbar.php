50b98e0c837ae34a77fa83d9bd8c579f6192dc51
Galen Strasen <galen@wearetmbr.com>
2016-11-03 17:26:36 -0700

    Navbar markup and style

---
+++ wp-content/themes/shespeaks/partials/03_organism/navbar.php
@@ -0,0 +1,25 @@
+<section class="pg-section section-nav ">
+  <div class="navbar js-nav-guy">
+    <div class="wrapper">
+    <div class="brand">
+      <h3 class="init"><a href="/">GS</a></h3>
+    </div><!-- /brand -->
+
+    <ul class="links plus">
+      <li><a href="#hello" class="scroll-to">Hello</a></li>
+      <li><a href="/cv">CV</a></li>
+      <li class="hidden"><a href="">Work</a>
+        <ul class="hidden">
+          <li><a href="">Recent</a></li>
+          <li><a href="">Archives</a></li> <!-- page -->
+          <li><a href="">Blog</a></li> <!-- page -->
+        </ul>
+      </li>
+      <li><a href="">Connect</a></li> <!-- footer -->
+      <li><a href="/styles">Styles</a></li>
+      <li><a href="#top-of-the-world" class="scroll-to">Top</a></li> <!-- top of everypage -->
+    </ul>
+  </div>
+  </div>
+
+</section><!-- /section-nav -->
